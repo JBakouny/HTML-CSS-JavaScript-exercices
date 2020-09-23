d43b44c96f3d2253a8798799fe8ac3df6fc2809b
Joseph Bakouny <Youssef.Bakouny@usj.edu.lb>
2020-09-23 09:14:06 +0300

    Add a test form

--- Test.js
+++ Test.js
@@ -1,3 +1,13 @@
 function surprise(n) {
     alert("Hello!" + n);
 }
+
+function getName() {
+    var name = prompt('Enter your name:')
+    return name;
+}
+
+function writeName() {
+    var name = getName()
+    //document.write("Bonjour " + name)
+}
